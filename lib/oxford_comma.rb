7387d9e979823139f223ff0622f0234e05034d66
Madelyn Deshield <madelynwallop@gmail.com>
2019-11-05 04:41:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,8 @@ def oxford_comma(array)
    if array.size == 1 
      return array.first
      elsif array.size == 2
-    return array.first + " and " + array.last  
+   # return array.first + " and " + array.last  
+   return array.join(" and ")
   else 
     last = array.pop
     return array.join(", ") + ", and " + last
