553e84359aa38bee30362176ca926b2347a4dfb7
Madelyn Deshield <madelynwallop@gmail.com>
2019-11-05 04:47:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+   
+   if array.size == 1 
+     return array.first
+     elsif array.size == 2
+   # return array.first + " and " + array.last  
+   return array.join(" and ")
+  else 
+    last = array.pop
+    return array.join(", ") + ", and " + last
+ end
 end
